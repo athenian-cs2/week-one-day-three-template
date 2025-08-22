0fd16bbedca9be5198a8b28d83df7cf8c194ddaa
sthompson-athenian <sthompson@athenian.org>
2025-08-22 16:05:58 -0700

    Update Gradle to 7.6, Java to 17, and apply code formatting

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,55 +1,55 @@
 public class MyMain {
-    // Adds 5 to the given integer
-    // This method has been written for you
-    public static int add5(int x) {
-        return (x + 5);
-    }
+  // Adds 5 to the given integer
+  // This method has been written for you
+  public static int add5(int x) {
+    return (x + 5);
+  }
 
-    // Given three ints x, y, and z, this method
-    // returns the largest of the three numbers
-    public static int findBiggestNumber(int x, int y, int z) {
-        // REPLACE THIS WITH YOUR CODE
-        return 0;
-    }
+  // Given three ints x, y, and z, this method
+  // returns the largest of the three numbers
+  public static int findBiggestNumber(int x, int y, int z) {
+    // REPLACE THIS WITH YOUR CODE
+    return 0;
+  }
 
-    // This method simulates the flip of a coin, where
-    // true represents "heads" and false represents "tails"
-    // This method should return true half the time, and false the other half
-    public static boolean flipCoin() {
-        // REPLACE THIS WITH YOUR CODE
-        return false;
-    }
+  // This method simulates the flip of a coin, where
+  // true represents "heads" and false represents "tails"
+  // This method should return true half the time, and false the other half
+  public static boolean flipCoin() {
+    // REPLACE THIS WITH YOUR CODE
+    return false;
+  }
 
-    // This method simulates the flip of a weighted coin,
-    // that is a coin that is biased towards heads
-    public static boolean flipWeightedCoin() {
-        // REPLACE THIS WITH YOUR CODE
-        return false;
-    }
+  // This method simulates the flip of a weighted coin,
+  // that is a coin that is biased towards heads
+  public static boolean flipWeightedCoin() {
+    // REPLACE THIS WITH YOUR CODE
+    return false;
+  }
 
-    // This method rounds a double to the nearest whole number
-    public static int round(double d) {
-        // REPLACE THIS WITH YOUR CODE
-        return 0;
-    }
+  // This method rounds a double to the nearest whole number
+  public static int round(double d) {
+    // REPLACE THIS WITH YOUR CODE
+    return 0;
+  }
 
-    // This method returns a random number between 1 and 6, inclusive
-    public static int rollDie() {
-        // REPLACE THIS WITH YOUR CODE
-        return 0;
-    }
+  // This method returns a random number between 1 and 6, inclusive
+  public static int rollDie() {
+    // REPLACE THIS WITH YOUR CODE
+    return 0;
+  }
 
-    // This method carries out the quadratic formula and *prints* out the answers
-    public static void quadForm(int a, int b, int c) {
-        return;
-    }
+  // This method carries out the quadratic formula and *prints* out the answers
+  public static void quadForm(int a, int b, int c) {
+    return;
+  }
 
-    public static void main(String[] args) {
-        // Test the add5 method
-        int x = 2;
-        int y = add5(x);
-        System.out.println(y);
+  public static void main(String[] args) {
+    // Test the add5 method
+    int x = 2;
+    int y = add5(x);
+    System.out.println(y);
 
-        // YOUR CODE HERE
-    }
+    // YOUR CODE HERE
+  }
 }
